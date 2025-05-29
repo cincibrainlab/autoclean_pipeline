87f77d34a6bb287fb286ed321b511c8f403f8bbe
github-action-benchmark <github@users.noreply.github.com>
2025-05-29 12:08:49 +0000

    add Benchmark (pytest) benchmark result for 537fb2da98e97d48faed26edc63f78cf4ee0eaa1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748518740600,
+  "lastUpdate": 1748520529068,
   "repoUrl": "https://github.com/cincibrainlab/autoclean_pipeline",
   "entries": {
     "Benchmark": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0017132620442705095",
             "extra": "mean: 164.17352950000463 msec\nrounds: 6"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gammohgb@mail.uc.edu",
+            "name": "Gavin Gammoh",
+            "username": "ggammoh"
+          },
+          "committer": {
+            "email": "gammohgb@mail.uc.edu",
+            "name": "Gavin Gammoh",
+            "username": "ggammoh"
+          },
+          "distinct": true,
+          "id": "537fb2da98e97d48faed26edc63f78cf4ee0eaa1",
+          "message": "Cleanup",
+          "timestamp": "2025-05-29T08:06:18-04:00",
+          "tree_id": "380a39210942e85ac860a4bfd5525eeb3a7d8706",
+          "url": "https://github.com/cincibrainlab/autoclean_pipeline/commit/537fb2da98e97d48faed26edc63f78cf4ee0eaa1"
+        },
+        "date": 1748520527320,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "performance/benchmark_eeg_processing.py::TestEEGProcessingBenchmarks::test_synthetic_data_generation_performance",
+            "value": 6.08260406855772,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000656279313761158",
+            "extra": "mean: 164.4032701666731 msec\nrounds: 6"
           }
         ]
       }
